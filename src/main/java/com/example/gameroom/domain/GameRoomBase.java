84f1937781d05838b42466d1ff7688d926e0f171
G0sim <angela07697@gmail.com>
2024-11-16 16:11:30 +0900

    GameRoom Domain

--- src/main/java/com/example/gameroom/domain/GameRoomBase.java
+++ src/main/java/com/example/gameroom/domain/GameRoomBase.java
@@ -2,3 +2,8 @@ package com.example.gameroom.domain;
 
 public class GameRoomBase {
 }
+
+
+
+
+//id 게임방 id 게임룰 보상id 게임 종류 최대 플레이어 수 턴수 턴 당 제한 시간 플레이어당 시드 머니 게임 당 기업 수 룰 적용 여부
--- src/main/java/com/example/gameroom/domain/GameRooms.java
+++ src/main/java/com/example/gameroom/domain/GameRooms.java
@@ -1,4 +1,45 @@
 package com.example.gameroom.domain;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Builder
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class GameRooms {
+
+    @Id
+    @Column(name="game_room_id")
+    private String id;
+    @Enumerated(EnumType.STRING)
+    @Column(name="game_status")
+    private GameStatus status;
+    @Column(name="end_time")
+    private LocalDateTime endTime;
+    @Column(name="current_turn")
+    private int turn;
+
+    public enum GameStatus {
+        BEFORE_START,
+        IN_PROGRESS,
+        COMPLETED
+    }
+
+
+    @Builder
+    public GameRooms(GameStatus status, LocalDateTime endTime, int turn) {
+        this.id = UUID.randomUUID().toString().substring(0,5);
+        this.status = status;
+        this.endTime = endTime;
+        this.turn = turn;
+    }
+
 }
